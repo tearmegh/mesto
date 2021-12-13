e96b31cbf58c784521bd70f364ab2fc483fdd8c4
Daria Kornilova <ashkornen@ya.ru>
2021-12-13 18:25:47 +0200

    first review fix

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,31 @@
-const editForm = document.querySelector(".profile__edit-form");
+const editForm = document.querySelector(".popup");
 const anchor = document.querySelector(".profile__edit-btn");
-const closeEditForm = document.querySelector(".profile__close-btn");
-const overlay = document.querySelector(".page__overlay");
+const closeEditForm = document.querySelector(".popup__close-btn");
+const overlay = document.querySelector(".popup__overlay");
 let nameText = document.querySelector(".profile__name");
 let captionText = document.querySelector(".profile__caption");
-let nameInput = document.querySelector(".profile__name-input");
-let captionInput = document.querySelector(".profile__caption-input");
-nameInput.value = nameText.textContent;
-captionInput.value = captionText.textContent;
+let nameInput = document.querySelector(".popup__input_type_name");
+let captionInput = document.querySelector(".popup__input_type_caption");
 function openEditForm () {
-  editForm.classList.add("profile__edit-form_active");
+  editForm.classList.add("popup_active");
   document.body.classList.add("hidden");
-  overlay.classList.add("page__overlay_active");
+  overlay.classList.add("popup__overlay_active");
+  nameInput.value = nameText.textContent;
+  captionInput.value = captionText.textContent;
 }
-anchor.addEventListener("click", openEditForm);
 function closeForm () {
-  editForm.classList.remove("profile__edit-form_active");
+  editForm.classList.remove("popup_active");
   document.body.classList.remove("hidden");
-  overlay.classList.remove("page__overlay_active");
+  overlay.classList.remove("popup__overlay_active");
 }
-closeEditForm.addEventListener("click", closeForm);
-overlay.addEventListener("click", closeForm);
 function formSubmitHandler (evt) {
   evt.preventDefault();
   nameText.innerText = nameInput.value;
   captionText.innerText = captionInput.value;
   closeForm();
 }
-addEventListener("submit", formSubmitHandler);
+editForm.addEventListener("submit", formSubmitHandler);
+anchor.addEventListener("click", openEditForm);
+closeEditForm.addEventListener("click", closeForm);
+overlay.addEventListener("click", closeForm);
+
